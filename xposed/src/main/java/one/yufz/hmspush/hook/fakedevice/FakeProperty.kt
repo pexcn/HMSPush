e61cba0b16a5fd364403466ca77c794b816d5b96
pexcn <pexcn97@gmail.com>
2025-09-23 16:11:25 +0800

    update hook properties
    source: https://github.com/getActivity/AndroidSystemPropertyCollect/blob/8190032/HarmonyOS/HarmonyOS%204.3.0%20Android%2012%20HUAWEI%20Mate%2070.txt

--- xposed/src/main/java/one/yufz/hmspush/hook/fakedevice/FakeProperty.kt
+++ xposed/src/main/java/one/yufz/hmspush/hook/fakedevice/FakeProperty.kt
@@ -8,8 +8,8 @@ import java.util.concurrent.atomic.AtomicBoolean
 private const val TAG = "FakeProperties"
 
 enum class Property(val entry: Pair<String, String>) {
-    EMUI_API("ro.build.hw_emui_api_level" to "21"),
-    EMUI_VERSION("ro.build.version.emui" to "EmotionUI_8.0.0"),
+    EMUI_API("ro.build.hw_emui_api_level" to "35"),
+    EMUI_VERSION("ro.build.version.emui" to "EmotionUI_15.0.0"),
     BRAND("ro.product.brand" to "Huawei"),
     MANUFACTURER("ro.product.manufacturer" to "HUAWEI"),
     MIUI_VERSION("ro.miui.ui.version.name" to "");
